195a3e12b3e6a6f1cbf9d475858f3e23756cff36
Terkwood <metaterkhorn@gmail.com>
2019-05-13 14:13:23 -0400

    Stub out function

---
+++ src/main/kotlin/Producer.kt
@@ -0,0 +1,13 @@
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
+import java.util.Properties
+
+private fun createProducer(brokers: String): Producer<String, String> {
+    val props = Properties()
+    props["bootstrap.servers"] = brokers
+    props["key.serializer"] = StringSerializer::class.java.canonicalName
+    props["value.serializer"] = StringSerializer::class.java.canonicalName
+    return KafkaProducer<String, String>(props)
+}
